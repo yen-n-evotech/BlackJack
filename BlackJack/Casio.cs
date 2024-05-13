3930e11324ea6eb3a12359bfb56258edd2e8cfa9
yen-n <yen-n@evoltech.co.jp>
2024-05-13 10:21:43 +0900

    「"y"または"n"を入力しても実行ない」というバグを修正

--- BlackJack/Casio.cs
+++ BlackJack/Casio.cs
@@ -9,9 +9,9 @@ namespace BlackJack
 {
     internal class Casio //ゲームの進行を管理
     {
-        private Deck deck;  
-        private Player dealer;　
-        private Player player;　
+        private Deck deck;
+        private Player dealer;
+        private Player player;
 
         public Casio()
         {
@@ -25,12 +25,12 @@ namespace BlackJack
         //ゲームをスタート
         public void StartGame()
         {
-            Console.WriteLine("ゲームを開始します");
+            Console.WriteLine("\nゲームを開始します");
 
             //playerと dealerはカードを引く
             player.AddCardToHand(deck.GetCards());
             player.AddCardToHand(deck.GetCards());
-            dealer.AddCardToHand(deck.GetCards());           
+            dealer.AddCardToHand(deck.GetCards());
             dealer.AddCardToHand(deck.GetCards());
 
             //プレーヤーの手札を表示
@@ -43,30 +43,37 @@ namespace BlackJack
             playerCard = playerCard.Remove(playerCard.Length - 1);
             Console.WriteLine(playerCard);
 
+            int value = player.GetHandValue();
+            Console.WriteLine($"あなたの現在の得点は{value}です。\n");
+
             //ディーラーの手札を表示
             Console.Write("ディーラーの引いたカード: ");
             Console.WriteLine($"{dealer.Hand[0].Type}の{dealer.Hand[0].Rank}");
-            Console.WriteLine("ディーラーの2枚目のカードは分かりません。");
+            Console.WriteLine("ディーラーの2枚目のカードは裏向きです。");
 
+            SetPlayerTurn();            
+            GetResults();
+            ResetGame();
+        }
+        //プレーヤーのターン
+        public void SetPlayerTurn()
+        {
             int value = player.GetHandValue();
-            Console.WriteLine($"あなたの現在の得点は{value}です。");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("カードを引きますか。引く場合は Y を、引かない場合は N を入力してください！");
 
-            //プレーヤーのターン
+            //プレーヤーがヒットするかスタンドするかを選択
             while (true)
-            {
-                //プレーヤーがヒットするかスタンドするかを選択
-                Console.WriteLine("-------------------------------------------------");
-                Console.WriteLine("カードを引きますか。引く場合は Y を、引かない場合は N を入力してください！");
+            {                
                 string choice = Console.ReadLine().ToLower();
-
-                if (choice == "y" ) //ヒット
+                if (choice == "y") //ヒット
                 {
                     //カードを引いてdeckに追加
                     Card card = deck.GetCards();
                     player.AddCardToHand(card);
 
                     //プレーヤーが引いたカードを表示
-                    Console.WriteLine($"あなたが引いたカード:　{card.Type}の{card.Rank}");
+                    Console.WriteLine($"あなたが引いたカード: {card.Type}の{card.Rank}");
 
                     value += card.Value;
                     Console.WriteLine($"あなたの現在の得点は{value}です。");
@@ -76,39 +83,69 @@ namespace BlackJack
                     {
                         break;
                     }
+                    else
+                    {
+                        Console.WriteLine("-------------------------------------------------");
+                        Console.WriteLine("カードを引きますか。引く場合は Y を、引かない場合は N を入力してください！");
+                    }
                 }
 
                 else if (choice == "n")　//スタンド
                 {
+                    Console.WriteLine("-------------------------------------------------");
+                    Console.WriteLine("あなたの番が終了したのでディーラーの番になります。\r\nディーラーは得点が17点以上になるまでカードを引きます。");
+                    SetDealerTurn();
                     break;
+                }                
+            }
+        }
+
+        //ディーラーのターン
+        public void SetDealerTurn()
+        {
+           int value = dealer.GetHandValue();
+            Console.WriteLine($"\n裏向きの2枚目のカードは{dealer.Hand[1].Type}の{dealer.Hand[1].Rank}でした。");
+            Console.WriteLine($"ディーラーの現在の得点は{value}です。");
+            while (true)
+            {
+                
+                if (dealer.GetHandValue() < 17)
+                {
+                    Card card = deck.GetCards();
+                    dealer.AddCardToHand(card);
+                    
+                    //ディーラーが引いたカードを表示
+                    Console.WriteLine($"ディーラーが引いたカード: {card.Type}の{card.Rank}");
+
+                    value += card.Value;
+                    Console.WriteLine($"ディーラーの現在の得点は{value}です。");
                 }
                 else
                 {
-                    Console.WriteLine("無効な選択です。");
+                    Console.WriteLine("17点以上になったため、ディーラーの番を終了します。");
+                    break;
                 }
             }
+        }
 
-            //ディーラーのターン
-            while (dealer.GetHandValue() < 17)
-            {
-                Card card = deck.GetCards();
-                dealer.AddCardToHand(card);
-            }
-
-            //プレーヤーとディーラーの手札を表示
-            Console.WriteLine("-------------------------------------------------");
-            Console.Write("あなたのカード:　");
-            playerCard = "";
+        //プレーヤーとディーラーの手札を表示
+        public void GetResults()
+        {
+            Console.WriteLine("\r\nゲームが終了しました。\r\n");
+            Console.WriteLine("結果\r\n-------------------------------------------------");
+            Console.WriteLine("あなた");
+            Console.Write("  カード: ");
+            string playerCard = "";
             foreach (Card card in player.Hand)
             {
-                playerCard += $"{card.Type}の{card.Rank}、" ;            
+                playerCard += $"{card.Type}の{card.Rank}、";
             }
             playerCard = playerCard.Remove(playerCard.Length - 1);
             Console.WriteLine(playerCard);
-            Console.WriteLine($"あなたの得点：{player.GetHandValue()}点");
+            Console.WriteLine($"  得点  ： {player.GetHandValue()}点");
 
-            Console.WriteLine("-------------------------------------------------");
-            Console.Write("ディーラーのカード:　");
+            Console.WriteLine("-------------------------------------------------\r\nディーラー");
+            Console.Write("  カード: ");
             string dealerCard = "";
             foreach (Card card in dealer.Hand)
             {
@@ -116,7 +153,9 @@ namespace BlackJack
             }
             dealerCard = dealerCard.Remove(dealerCard.Length - 1);
             Console.WriteLine(dealerCard);
-            Console.WriteLine($"ディーラーの得点：{dealer.GetHandValue()}点");
+            Console.WriteLine($"  得点  ： {dealer.GetHandValue()}点");
+            Console.WriteLine("-------------------------------------------------");
+
 
             //勝敗を判断
             if (player.GetHandValue() > 21)
@@ -127,7 +166,7 @@ namespace BlackJack
             {
                 Console.WriteLine("あなたの勝ちです！");
             }
-            else if (player.GetHandValue() > dealer.GetHandValue() )
+            else if (player.GetHandValue() > dealer.GetHandValue())
             {
                 Console.WriteLine("あなたの勝ちです！");
             }
@@ -139,7 +178,10 @@ namespace BlackJack
             {
                 Console.WriteLine("あなたの負けです！");
             }
+        }
 
+        public void ResetGame()
+        { 
             //手札をリセット
             player.ResetHand();
             dealer.ResetHand();
